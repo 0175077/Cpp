f69a06b60f756720a899ad3674b80666b40b57b1
0175077 <0175077@naver.com>
2025-11-14 15:47:56 +0900

    enum

---
+++ 1114/cpp/moveMgr.cpp
@@ -0,0 +1,29 @@
+﻿#include "moveMgr.h"
+#include "walk.h"
+#include "fly.h"
+
+void C_MOVEMGR::init()
+{
+	m_arMove[(int)E_TYPE::E_WALK] = new C_WALK{};
+	m_arMove[(int)E_TYPE::E_FLY] = new C_FLY{};
+
+}
+
+
+void C_MOVEMGR::release()
+{
+	for (int i = 0; i < (int)E_TYPE::E_MAX; i++)
+	{
+		if (m_arMove[i])
+		{
+			delete m_arMove[i];
+			m_arMove[i] = nullptr;
+		}
+	}
+}
+
+C_MOVE* C_MOVEMGR::getMove(E_TYPE eType)
+{
+	return m_arMove[(int)eType];
+}
+ 
