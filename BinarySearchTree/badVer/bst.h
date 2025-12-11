5482adca9acf0848ab41eceff3b692a3870795b5
0175077 <0175077@naver.com>
2025-12-11 15:44:08 +0900

    bst

---
+++ BinarySearchTree/badVer/bst.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include<stdio.h>
+
+class C_BST
+{
+private:
+	struct S_NODE
+	{
+		int nData;
+		S_NODE* pL;
+		S_NODE* pR;
+	};
+
+
+private:
+	S_NODE* m_pRoot;
+
+	void printNode(S_NODE* pNode);
+	S_NODE* createNode(int nData);
+public:
+	C_BST() = default;
+	bool insert(int nData);
+
+	void print();
+};
