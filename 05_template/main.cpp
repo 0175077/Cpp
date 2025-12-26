d4c591e6c88fd3c824a51a800c643a50fbb2370f
0175077 <0175077@naver.com>
2025-12-26 15:49:26 +0900

    template
    template

---
+++ 05_template/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+template<int nLength>
+void printData(const int(&arData)[nLength]);
+
+int main()
+{
+	int arData1[5]{ 5,4,3,2,1 };
+	int arData2[3]{ 3,2,1 };
+	int arData3[7]{ 7,6,5,4,3,2,1 };
+
+	// 이렇게 쓰지말고
+	printData(arData3);
+
+	// 이렇게 쓰자
+	printData<7>(arData3);
+
+
+}
+
+template<int nLength>
+void printData(const int(&arData)[nLength])
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", arData[i]);
+	}
+	printf("\n");
+}
