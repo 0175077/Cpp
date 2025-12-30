e89f3e361ec1c2d8663b3c8db337f4a9c820307e
0175077 <0175077@naver.com>
2025-12-30 15:47:31 +0900

    Create main.cpp

---
+++ 06_iterator(std)/print point/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+void print(list<int>* pList);
+
+int main()
+{
+    list<int> list{};
+    list.push_back(5);
+    list.push_back(3);
+    list.push_back(3);
+    list.push_back(3);
+    list.push_back(7);
+
+    print(&list);
+}
+
+void print(list<int>* pList)
+{
+    list<int>::iterator iter = pList->begin();
+
+    while (iter != pList->end())
+    {
+        printf("%d ", *iter);
+        iter++;
+    }
+    printf("\n");
+}
